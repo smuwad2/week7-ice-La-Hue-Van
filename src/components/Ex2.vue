8462ea9b2a71cd7bec0af050f8edba673333db48
La Hue Van <huevan.la.2024@smu.edu.sg>
2025-10-21 05:15:14 +0000

    exercise 3 updated

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,16 +3,30 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem:'',
+            }},
+            methods: {
+                addItem(){
+                    this.items.push(this.newItem);
+                }
             }
             
+            
         }
-    }
+
+    
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for ='(item,idx) in items'>{{item}} <button @click='items.splice(idx,1)'>Delete</button></li>
+    </ul>
+    <input type="text" v-model="newItem">
+    <button @click="addItem">Add</button>
+    
    
 </template>
